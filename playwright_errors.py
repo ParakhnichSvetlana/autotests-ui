93149aca03a15415fc573c7ac7a984951fde2739
ParakhnichSvetlana <s-lashkina@yandex.ru>
2026-03-06 20:05:04 +0300

    Работа с авторизацией в Playwright

--- playwright_errors.py
+++ playwright_errors.py
@@ -8,3 +8,8 @@ with  sync_playwright() as playwright:
 
     unknown = page.locator("#unknown")
     expect(unknown).to_be_visible()
+
+    login_button = page.get_by_test_id('login-page-login-button')
+    login_button.fill('unknown')
+
+    
--- playwright_registration.py
+++ playwright_registration.py
@@ -2,7 +2,8 @@ from playwright.sync_api import sync_playwright, expect
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()  # Создание контекста
+    page = context.new_page()  # Создание страницы
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
@@ -21,5 +22,16 @@ with sync_playwright() as playwright:
     dashboard_toolbar = page.locator('//h6[@data-testid="dashboard-toolbar-title-text"]')
     expect(dashboard_toolbar).to_be_visible()
     expect(dashboard_toolbar).to_have_text('Dashboard')
+
+    context.storage_state(path="browser-state.json")
+
+    page.wait_for_timeout(5000)
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json")  # Указываем файл с сохраненным состоянием
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
 
     page.wait_for_timeout(5000)
